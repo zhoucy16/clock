1875d3978e0814347d4d3b898b779b9af457d448
Richard <zhoucy16@163.com>
2018-06-09 18:37:11 +0800

    整合了一下

--- config.js
+++ config.js
@@ -3,26 +3,36 @@
  */
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
-var host = 'https://sgoysnvt.qcloud.la';
+//var host = 'https://sgoysnvt.qcloud.la';
+var host = 'https://fhao.applinzi.com';
 
 var config = {
 
-    // 下面的地址配合云端 Demo 工作
-    service: {
-        host,
+  // 下面的地址配合云端 Demo 工作
+  service: {
+    host,
 
-        // 登录地址，用于建立会话
-        loginUrl: `${host}/login.php`,
+    // 登录地址，用于建立会话
+    loginUrl: `${host}/login.php`,
 
-        // 用于创建todo
-        createTodoUrl: `${host}/newTodo.php`,
+    // 用于创建todo
+    createTodoUrl: `${host}/newTodo.php`,
 
-        // 用于直接创建番茄并且启动：
-        createTomatoUrl: `${host}/newTomato.php`,
+    // 用于直接创建番茄并且启动：
+    createTomatoUrl: `${host}/newTomato.php`,
 
-        updateTomatoUrl: `${host}/updateTomato.php`,
+    updateTomatoUrl: `${host}/updateTomato.php`,
 
-    }
+    pauseTomatoUrl: `${host}/pauseTomato.php`,
+
+    stopTomatoUrl: `${host}/stopTomato.php`,
+
+    getTodoUrl: `${host}/getTodo.php`,
+
+    startTodoUrl: `${host}/startTodo.php`,
+
+    getUserInfoUrl: `${host}/getUserInfo.php`,
+  }
 };
 
 module.exports = config;
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,13 +6,14 @@ Page({
         userInfo: {},
         logged: false, 
         returnedData: {},
-        clockInfo: "开始番茄",
+        clockInfo: "开始番茄！",
         tomatoTime: 25,
         restTime: 5,
-        todoForm: 1
+        // 默认是倒计时
+        todoForm: 2
     },
 
-    // 登录
+    // 登录（此时会检查并且返回番茄状态，如果番茄过期则会返回的data.tomatoInfo.state="passedAndPaused"，可以弹出一个提示询问是否继续，继续则调用pauseOperation即可
     login: function() {
       var that = this;
       wx.login({
@@ -28,6 +29,7 @@ Page({
                 'content-type': 'application/json'
               },
               success: function (res) {
+                res.data = JSON.parse(res.data.split("<script")[0]);
                 console.log("已登陆！返回信息：", res.data);
                 wx.setStorageSync('session', res.data.session);
                 wx.setStorageSync('invite', res.data.invite);
@@ -45,7 +47,8 @@ Page({
       })
     },
 
-    testSubmit: function(e) {
+    // 这个是按下创建按钮时；现在正计时可能没有时间做了
+    createTodoNow: function(e) {
       e.detail.value['session'] = wx.getStorageSync('session');
       e.detail.value['invite'] = wx.getStorageSync('invite');
 
@@ -57,12 +60,13 @@ Page({
           'content-type': 'application/json'
         },
         success: function (res) {
+          res.data = JSON.parse(res.data.split("<script")[0]);
           console.log("已创建！返回信息：", res.data);
-          
         }
       })
     },
 
+    // 直接点击开始番茄按钮，通过data里面的设置来设定状态
     clockOperation: function() {
       wx.request({
         url: config.service.createTomatoUrl,
@@ -77,11 +81,13 @@ Page({
           'content-type': 'application/json'
         },
         success: function (res) {
+          res.data = JSON.parse(res.data.split("<script")[0]);
           console.log("已创建！返回信息：", res.data);
         }
       })
     },
 
+    // 一个用来同步时间的
     updateOperation: function() {
       wx.request({
         url: config.service.updateTomatoUrl,
@@ -93,9 +99,109 @@ Page({
           'content-type': 'application/json'
         },
         success: function (res) {
+          res.data = JSON.parse(res.data.split("<script")[0]);
           console.log("已更新！返回信息：", res.data);
         }
       })
-    }
+    },
+
+    // 暂停按钮，虽然我觉得既然要番茄就是不想要暂停的啊……无所谓了反正都是在咸鱼
+    pauseOperation: function() {
+      wx.request({
+        url: config.service.pauseTomatoUrl,
+        data: {
+          'session': wx.getStorageSync('session'),
+          'invite': wx.getStorageSync('invite'),
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          res.data = JSON.parse(res.data.split("<script")[0]);
+          console.log("已暂停或恢复！返回信息：", res.data);
+        }
+      })
+    },
+
+    // 停止按钮，定了番茄又停止可太咸鱼了
+    stopOperation: function () {
+      wx.request({
+        url: config.service.stopTomatoUrl,
+        data: {
+          'session': wx.getStorageSync('session'),
+          'invite': wx.getStorageSync('invite'),
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          res.data = JSON.parse(res.data.split("<script")[0]);
+          console.log("已停止！返回信息：", res.data);
+        }
+      })
+    },
+
+    // 查找todo的按钮
+    // 注意：在之前的开始、更新、暂停、停止操作中，都已经返回了todoInfo，在这时应该保存（如果不想保存的话就在后端别返回这些了）
+    // 所以应该是基于这些来进行的
+    getTodoSubmit: function (e) {
+      e.detail.value['session'] = wx.getStorageSync('session');
+      e.detail.value['invite'] = wx.getStorageSync('invite');
+
+      console.log("查找todo：", e.detail.value)
+      wx.request({
+        url: config.service.getTodoUrl,
+        data: e.detail.value,
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          res.data = JSON.parse(res.data.split("<script")[0]);
+          console.log("已查询！返回信息：", res.data);
+        }
+      })
+    },
+
+    // 执行todo的按钮
+    startTodoSubmit: function (e) {
+      e.detail.value['session'] = wx.getStorageSync('session');
+      e.detail.value['invite'] = wx.getStorageSync('invite');
+
+      console.log("执行todo：", e.detail.value)
+      wx.request({
+        url: config.service.startTodoUrl,
+        data: e.detail.value,
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          res.data = JSON.parse(res.data.split("<script")[0]);
+          console.log("已执行！返回信息：", res.data);
+        }
+      })
+    },
+
+    // 查找userinfo的按钮
+    // 注意：之前只有登陆的时候返回了userinfo，但是中间的完成番茄操作会改变userinfo，所以记得要改！
+    // 注意：一天的定义是从早上3点到第二天早上三点！
+    getUserInfoSubmit: function (e) {
+      e.detail.value['session'] = wx.getStorageSync('session');
+      e.detail.value['invite'] = wx.getStorageSync('invite');
+
+      console.log("查找UserInfo：", e.detail.value)
+      wx.request({
+        url: config.service.getUserInfoUrl,
+        data: e.detail.value,
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          res.data = JSON.parse(res.data.split("<script")[0]);
+          console.log("已查询UserInfo！返回信息：", res.data);
+        }
+      })
+    },
+
+
 
 })
